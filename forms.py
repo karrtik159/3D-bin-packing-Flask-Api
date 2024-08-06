97304b249e82d04786c3ea16487d37de2b031af8
tkmptech <168623050+tkmptech@users.noreply.github.com>
2024-08-06 11:50:12 +0530

    Readme Update

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField, SubmitField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms.validators import DataRequired, NumberRange,InputRequired
 
 class BoxForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
@@ -16,7 +16,7 @@ class ItemForm(FlaskForm):
     count = IntegerField('Count', validators=[DataRequired(), NumberRange(min=1)])
     updown = SelectField('Updown', choices=[(1, 'Yes'), (0, 'No')], coerce=int)
     type = SelectField('Type', choices=[(1, 'Type 1'), (2, 'Type 2')], coerce=int)
-    level = IntegerField('Level', validators=[DataRequired()])
+    level = IntegerField('Level', validators=[InputRequired()])
     loadbear = IntegerField('Loadbear', validators=[DataRequired(), NumberRange(min=1)])
     weight = IntegerField('Weight', validators=[DataRequired(), NumberRange(min=1)])
     color = SelectField('Color', choices=[(1, 'Red'), (2, 'Blue'), (3, 'Green'), (4, 'Yellow'), (5, 'Black'), (6, 'White'), (7, 'Brown')], coerce=int)
